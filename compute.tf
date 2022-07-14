123c375f20917cdea2d0dc9a118b851ec57fd2ab
joshking1 <88409463+joshking1@users.noreply.github.com>
2022-07-14 12:04:18 -0400

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -43,11 +43,24 @@ resource "aws_security_group" "sg_allow_ssh_jenkins" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  ingress {
+    ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "http"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+    ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "https"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
