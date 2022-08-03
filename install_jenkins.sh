b35e88e82cb25449fb3ac9a9a9e1d794eb2521a4
joshking1 <88409463+joshking1@users.noreply.github.com>
2022-08-02 21:20:51 -0400

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -2,9 +2,9 @@
 sudo yum -y update
 
 echo "Install Java JDK 8"
-yum remove -y java
+sudo yum remove -y java
 #yum install -y java-1.8.0-openjdk
-#sudo yum install java-11-openjdk 
+sudo amazon-linux-extras install java-openjdk11
 
 echo "Install Maven"
 yum install -y maven 
