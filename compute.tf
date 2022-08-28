c15985d4893b42ac68481f24a2095f46440ac8cf
joshking1 <88409463+joshking1@users.noreply.github.com>
2022-08-28 15:48:29 -0400

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -14,19 +14,6 @@ data "aws_ami" "amazon-linux-2" {
       "amazon",
     ]
   }
-    most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "jenkins-instance" {
