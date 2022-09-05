64c23126a7870b4674a0ad7541b13b0cc601d904
joshking1 <88409463+joshking1@users.noreply.github.com>
2022-09-05 19:27:39 -0400

    Create alb.tf

---
+++ alb.tf
@@ -0,0 +1,46 @@
+# Creating External LoadBalancer
+resource "aws_lb" "external-alb" {
+  name               = "external-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = "${aws_security_group.sg_allow_ssh_jenkins.id}"
+  subnets            = "$[aws_subnet.public-subnet-1.id,aws_subnet.public-subnet-2.id]"
+}
+
+resource "aws_lb_target_group" "target-elb" {
+  name     = "target-elb"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${aws_vpc.development-vpc}"
+}
+
+resource "aws_lb_target_group_attachment" "attachment" {
+  target_group_arn = "${aws_lb_target_group.external-alb.arn}"
+  target_id        = "${aws_instance.jenkins-instance.id}"
+  port             = 80
+
+  depends_on = [
+    aws_instance.jenkins-instance,
+  ]
+}
+
+resource "aws_lb_target_group_attachment" "attachment1" {
+  target_group_arn = aws_lb_target_group.external-alb.arn
+  target_id        = "${aws_instance.jenkins-instance.id}"
+  port             = 80
+
+  depends_on = [
+    aws_instance.jenkins-instance,
+  ]
+}
+
+resource "aws_lb_listener" "external-elb" {
+  load_balancer_arn = aws_lb.external-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.external-alb.arn
+  }
+}
