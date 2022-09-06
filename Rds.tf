9e0aa71e63e95193c18fec27b2f4c4e68b8b45f8
joshking1 <88409463+joshking1@users.noreply.github.com>
2022-09-05 21:27:09 -0400

    Update Rds.tf

--- Rds.tf
+++ Rds.tf
@@ -16,7 +16,7 @@ resource "aws_db_instance" "default" {
   engine_version         = "8.0.28"
   instance_class         = "db.t2.micro"
   multi_az               = true
-  name                   = "Jenkins DB"
+  name                   = "Jenkins"
   username               = "username"
   password               = "password"
   skip_final_snapshot    = true
