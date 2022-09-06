a41680119b45c91827b307134dee14a05a452d92
joshking1 <88409463+joshking1@users.noreply.github.com>
2022-09-05 20:47:41 -0400

    Update Rds.tf

--- Rds.tf
+++ Rds.tf
@@ -1,7 +1,7 @@
 # Creating RDS Instance
 resource "aws_db_subnet_group" "default" {
   name       = "main"
-  subnet_ids = "${aws_subnet.public-subnet-1.id}"
+  subnet_id  = "${aws_subnet.public-subnet-1.id}"
 
   tags = {
     Name = "My DB subnet group"
